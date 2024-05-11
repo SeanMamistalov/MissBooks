d11f0c08e64c0a997faffe2b4f61afb137d7737e
Sean <mamistalovsean@gmail.com>
2024-05-11 14:26:07 +0300

    Create pages- about home and bookindex

--- RootCmp.jsx
+++ RootCmp.jsx
@@ -1,7 +1,35 @@
+import { BookIndex } from "./pages/BookIndex.jsx";
+import { AboutUs } from "./pages/About.jsx";
+import { Home } from "./pages/Home.jsx";
+
+const { useState } = React;
+
 export function RootCmp() {
+    const [route, setRoute] = useState("Books");
+  
     return (
+      <React.Fragment>
+        <header>
+          <h1>Miss Books</h1>
+          <nav>
+            <a onClick={() => setRoute("Home")} href="#">
+              Home
+            </a>
+            <a onClick={() => setRoute("About")} href="#">
+              About
+            </a>
+            <a onClick={() => setRoute("Books")} href="#">
+              Books
+            </a>
+          </nav>
+        </header>
+  
         <main className="content-grid">
-            <h1>Hello React</h1>
+          {route === "Home" && <Home />}
+          {route === "Books" && <BookIndex />}
+          {route === "About" && <AboutUs />}
         </main>
-    )
-}
+      </React.Fragment>
+    );
+  }
+  

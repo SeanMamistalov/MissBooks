77a3f092d8c3074c24c76b313149a43f9f22d7b5
Sean <mamistalovsean@gmail.com>
2024-05-18 18:00:02 +0300

    Create BookFilter

--- pages/BookIndex.jsx
+++ pages/BookIndex.jsx
@@ -1,20 +1,30 @@
-const { useState, useEffect } = React
+const { useState, useEffect } = React;
 
-import { bookService } from '../services/book.service.js'
-import { BookList } from '../cmps/BookList.jsx'
+import { bookService } from '../services/book.service.js';
+import { BookList } from '../cmps/BookList.jsx';
+import { BookFilter } from '../cmps/BookFilter.jsx';
 
 export function BookIndex() {
     const [books, setBooks] = useState([]);
+    const [filterBy, setFilterBy] = useState(bookService.getDefaultFilter());
   
     useEffect(() => {
-      bookService.query().then(books => setBooks(books));
-    }, []);
+        bookService.query(filterBy).then(books => setBooks(books));
+    }, [filterBy]);
   
+    function onSetFilterBy(newFilter) {
+        setFilterBy(newFilter)
+    }
+
+    function removeBook(bookId){  
+        bookService.remove(bookId)
+            .then(() => setBooks(prevBooks => prevBooks.filter(book => book.id !== bookId)))
+    }
+
     return (
-      <section className="book-index">
-        <h1>Books</h1>
-        <BookList books={books} />
-      </section>
+        <section className="book-index">
+          <BookFilter filterBy={filterBy} onFilter={onSetFilterBy}/>
+          {<BookList books={books} onRemove={removeBook}/>}
+        </section>
     );
-  }
-  
+}
